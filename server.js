4e892e6290fcf43a9a85fca07f07ef1ecef06c74
Amanda <amanda.waller.33@gmail.com>
2019-02-03 11:42:44 -0500

    fixed author POST (I think???)

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.get("/authors", (req, res) => {
 
 //----------------GOOD
 //----------------POST REQUEST (BLOG POSTS AND AUTHOR)
-app.post('/posts', (req, res) => {
+app.post("/posts", (req, res) => {
   const requiredFields = ["title", "content", "author_id"];
   for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
@@ -122,7 +122,7 @@ app.post('/posts', (req, res) => {
   });
 
 //----------------POST REQUEST (AUTHORS)
-app.post('/posts', (req, res) => {
+app.post("/authors", (req, res) => {
   const requiredFields = ["firstName", "lastName", "userName"];
   for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
@@ -130,16 +130,39 @@ app.post('/posts', (req, res) => {
       const message = `Missing \`${field}\` in request body`;
       console.error(message);
       return res.status(400).send(message);
-  }
-  
-  Authors
-    .findById(req.params.id)
-    .then(post => res.json(post.serialize()))
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ error: "something went terribly wrong" });
-    });
+    }
+  };
 
+  Author
+  .findOne({ userName: req.body.userName })
+  .then(author => {
+    if (author) {
+      const message = `Username already taken`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+    else {
+      Author
+        .create({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          userName: req.body.userName
+        })
+        .then(author => res.status(201).json({
+            _id: author.id,
+            name: `${author.firstName} ${author.lastName}`,
+            userName: author.userName
+          }))
+        .catch(err => {
+          console.error(err);
+          res.status(500).json({ error: "Something went wrong" });
+        });
+    }
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  });
 });
 
 //----------------GOOD
@@ -164,6 +187,46 @@ app.put("/posts/:id", (req, res) => {
     .then(updatedPost => res.status(204).end())
     .catch(err => res.status(500).json({ message: "something went terribly wrong" }));
 });
+
+//----------------PUT REQUEST (AUTHOR)
+app.put("/authors/:id", (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    res.status(400).json({
+      error: "Request path id and request body id values must match"
+    });
+  }
+
+  const updated = {};
+  const updateableFields = ["firstName", "lastName", "userName"];
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      updated[field] = req.body[field];
+    }
+  });
+
+  Author
+    .findOne({ userName: updated.userName || "", _id: { $ne: req.params.id } })
+    .then(author => {
+      if(author) {
+        const message = `Username is taken`;
+        console.error(message);
+        return res.status(400).send(message);
+      }
+      else {
+        Author
+          .findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
+          .then(updatedAuthor => {
+            res.status(200).json({
+              id: updatedAuthor.id,
+              name: `${updatedAuthor.firstName} ${updatedAuthor.lastName}`,
+              userName: updatedAuthor.userName
+            });
+          })
+          .catch(err => res.status(500).json({ message: err }));
+      }
+    });
+});
+
 
 //----------------GOOD
 //----------------DELETE REQUEST SPECIFIC POST
